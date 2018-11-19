aed11cdfa189a2be26ffaa6452014ffe34a74c0a
Ole Andreas Hansen <oleaha@me.com>
2018-11-19 21:09:25 +0100

    Update database.php

--- system/database.php
+++ system/database.php
@@ -5,33 +5,33 @@ require '../lib/medoo.min.php';
 if(PROD == true) {
     $count = new medoo(array(
         'database_type' => 'mysql',
-        'database_name' => 'individu.count',
-        'server' => 'localhost',
-        'username' => 'root',
-        'password' => 'Y-t5JMu73'
+        'database_name' => 'count',
+        'server' => '',
+        'username' => '',
+        'password' => ''
     ));
 
     $individu = new medoo(array(
         'database_type' => 'mysql',
-        'database_name' => 'individu.main',
-        'server' => 'localhost',
-        'username' => 'root',
-        'password' => 'Y-t5JMu73'
+        'database_name' => 'main',
+        'server' => '',
+        'username' => '',
+        'password' => ''
     ));
 } else {
     $count = new medoo(array(
         'database_type' => 'mysql',
-        'database_name' => 'individu.count',
-        'server' => '185.7.63.117',
-        'username' => 'root',
-        'password' => 'Y-t5JMu73'
+        'database_name' => 'count',
+        'server' => '',
+        'username' => '',
+        'password' => ''
     ));
 
     $individu = new medoo(array(
         'database_type' => 'mysql',
-        'database_name' => 'individu.main',
-        'server' => '185.7.63.117',
-        'username' => 'root',
-        'password' => 'Y-t5JMu73'
+        'database_name' => 'main',
+        'server' => '',
+        'username' => '',
+        'password' => ''
     ));
 }
